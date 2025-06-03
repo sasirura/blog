d4cbf751094958ab9f2602f4f587050ed97e155c
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2025-06-03 23:35:08 +0530

    revamp the ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,10 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
-
-import partytown from '@astrojs/partytown'
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.sasiru.eu.org/',
-	// Write here your website url
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			}
-		})
-	]
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
